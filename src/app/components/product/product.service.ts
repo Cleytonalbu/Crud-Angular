f4bdfb4e0b39cc1f6f7ff600b274536229873f9a
Cleyton Albuquerque <cleyton.albuquerque@dcx.ufpb.br>
2023-11-14 11:15:58 -0300

    feat: Exibindo produtos da API com #ngFor

--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -23,7 +23,12 @@ export class ProductService {
     })
   }
 
-  create (product: Product): Observable<Product>{
+  create(product: Product): Observable<Product> {
     return this.http.post<Product>(this.baseUrl, product)
   }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl)
+  }
+
 }
